4556ce0177af07b281af473e82fb5d5c1282fac2
Luiz Venturote <luiz@imaginemage.com>
2019-10-16 20:24:41 -0300

    Add SW Lifecycle

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
 'use strict';
+  
+addEventListener('install', function (event) {
+    console.log('INSTALL: The service worker is installing . . . ', event);
+});
+
+addEventListener('activate', function (event) {
+    console.log('ACTIVATE - The service worker is activated.', event);
+});
+
+addEventListener('fetch', function (event) {
+    //console.log('FETCH: The service worker is listening.', event);
+});
